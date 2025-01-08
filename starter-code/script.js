538f44ce68bce835bdf8b9dc3313401f74b6a1e8
Muskan <muskan.goyal@liferay.com>
2025-01-08 16:01:24 +0530

    updated footer and added js

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,6 +1,11 @@
-
-    document.querySelector('.membership-button').addEventListener('click', function () {
-        document.getElementById('membership-options').scrollIntoView({
-            behavior: 'smooth'
-        });
+document.getElementById('scroll-down').addEventListener('click', function () {
+    document.getElementById('membership-options').scrollIntoView({
+        behavior: 'smooth'
     });
+});
+
+document.getElementById('scroll-up').addEventListener('click', function () {
+    document.getElementById('membership-options').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
